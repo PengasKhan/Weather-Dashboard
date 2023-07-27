4204112d638d7fea027cad34fc71057185e00a81
PengasKhan <cheesepuff42@gmail.com>
2023-07-26 23:38:10 -0500

    Close but no cigar
    Api fetch works but I have some sort of error. will get it solved and reposted soon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,88 @@
-const weatherFormEl = document.querySelector('#weather-form')
-const cityInputEl = document.querySelector('#enterCity')
+const weatherFormEl = document.querySelector("#weather-form");
+const cityInputEl = document.querySelector("#enterCity");
+const containerEl = document.querySelector("#weather-container");
+const citySearchTerm = document.querySelector("#city-search-term");
 
 const formSubmitHandler = function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const city = cityInputEl.value.trim();
+  const city = cityInputEl.value.trim();
 
-    if (city)
-}
+  if (city) {
+    getLocation(city);
 
-const getCityWeather = function (cityLat, cityLon) {
-    const apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + cityLat + '&lon=' + cityLon + '&appid=62af28ccccd9db6d1b5c5a5bc32ebc7e';
-    
-    fetch(apiUrl)
+    containerEl.textContent = "";
+    cityInputEl.textContent = "";
+  } else {
+    alert("Please enter a city");
+  }
+};
+
+const getLocation = function (cityName) {
+  const locUrl =
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+    cityName +
+    "&limit=1&appid=62af28ccccd9db6d1b5c5a5bc32ebc7e";
+
+  fetch(locUrl).then(function (response) {
     if (response.ok) {
-        response.json().then(function (data) {
-          displayRepos(data.items, language);
-        });
-      } else {
-        alert('Error: ' + response.statusText);
-      }
-      .catch(function (error) {
-        alert('Unable to connect to OpenWeather');
+      response.json().then(function (data) {
+        getCityWeather(cityName, data[0].lat, data[0].lon);
       });
-}
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  });
+};
 
-weatherFormEl.addEventListener('submit', formSubmitHandler);
+const getCityWeather = function (locName, cityLat, cityLon) {
+  const weatherUrl =
+    "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+    cityLat +
+    "&lon=" +
+    cityLon +
+    "&appid=62af28ccccd9db6d1b5c5a5bc32ebc7e";
+
+  fetch(weatherUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        displayWeather(
+          locName,
+          data.list.main.temp,
+          data.list.weather.description,
+          data.list.wind.speed,
+          data.list.main.humidity
+        );
+      });
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  });
+};
+
+const displayWeather = function (name, temp, desc, windSpeed, humidity) {
+  citySearchTerm.textContent = name;
+  const weatherEl = document.createElement("div");
+  weatherEl.classList = "list-item flex-row justify-space-between align-center";
+
+  const tempEl = document.createElement("span");
+  tempEl.textContent = "The current temperature is " + temp + " Kelvin.";
+
+  const descEl = document.createElement("span");
+  descEl.textContent = desc;
+
+  const windEl = document.createElement("span");
+  windEl.textContent = "The current wind speed is " + windSpeed + "m/s.";
+
+  const humidityEl = document.createElement("span");
+  humidityEl.textContent = "The current humidity is " + humidity + "%";
+
+  weatherEl.appendChild(tempEl);
+  weatherEl.appendChild(descEl);
+  weatherEl.appendChild(windEl);
+  weatherEl.appendChild(humidityEl);
+
+  containerEl.appendChild(weatherEl);
+};
+
+weatherFormEl.addEventListener("submit", formSubmitHandler);
